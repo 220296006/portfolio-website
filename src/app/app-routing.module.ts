5d884e977c1bd34aaf5e41a5ccc94058728c774e
Thabiso Matsaba <Thabisomatsaba@capaciti.org.za>
2023-08-29 13:44:51 +0200

    Added about-me, education and resume components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
+import { EducationComponent } from './components/education/education.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { NavComponent } from './shared/nav/nav.component';
+import { AboutMeComponent } from './components/about-me/about-me.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '#', pathMatch: 'full', component:NavComponent},
+  {path: 'home', pathMatch: 'full', component: HomeComponent},
+  {path: 'education', pathMatch: 'full', component: EducationComponent},
+  {path: 'about-me', pathMatch: 'full', component: AboutMeComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { EducationComponent } from './components/education/education.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
+import { NavComponent } from './shared/nav/nav.component';
+import { AboutMeComponent } from './components/about-me/about-me.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    NavComponent,
+    AboutMeComponent,
+    EducationComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    BrowserAnimationsModule    
   ],
   providers: [],
   bootstrap: [AppComponent]
