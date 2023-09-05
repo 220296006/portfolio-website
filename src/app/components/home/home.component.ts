87d2bf8976df41ab48e998da6deec41321006313
Thabiso Matsaba <Thabisomatsaba@capaciti.org.za>
2023-09-05 16:13:27 +0200

    Fixing home page issues

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+
+const typed = document.querySelector('.typed') as HTMLElement | null;
+
+if (typed) {
+  let typed_strings = typed.getAttribute('data-typed-items') || '';
+  typed_strings = typed_strings.split(',');
+
+  new typed('.typed', {
+    strings: typed_strings,
+    loop: true,
+    typeSpeed: 100,
+    backSpeed: 50,
+    backDelay: 2000
+  });
+}
+
+  ngOnInit(): void {
+   
+  }
 
 }
