c0daddd43afa8f8d723dd0ca32adbf610ea96ed0
Thabiso Matsaba <Thabisomatsaba@capaciti.org.za>
2023-09-14 16:10:14 +0200

    Made changes to navigation bar to stick with scroll

--- src/app/shared/nav/nav.component.ts
+++ src/app/shared/nav/nav.component.ts
@@ -1,41 +1,26 @@
 import { Component, HostListener, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.scss']
 })
-export class NavComponent implements OnInit{
+export class NavComponent {
 
-  constructor( private router: Router){
-  }
+  isSticky: boolean = false;
 
-  @HostListener('window:scroll', ['$event'])
-  onScroll(event: Event): void {
     // Implement your scroll logic here
-    const scrollPosition = window.scrollY;
-
-    if (scrollPosition >= 300) {
-      this.activateNavLink('/home');
-      // Add other navigation link activations as needed
-    }
-  }
-
-  activateNavLink(route: string): void {
-    this.router.events.subscribe(() => {
-      const currentRoute = this.router.url;
-      if (currentRoute === route) {
-        // Mark the navigation link as active here
+    @HostListener('window:scroll', ['$event'])
+    checkScroll() {
+      const scrollPosition = window.scrollY;
+  
+      if (scrollPosition >= 100) {
+        this.isSticky = true;
+      } else {
+        this.isSticky = false;
       }
-    });
-  }
-
-  isNavLinkActive(route: string): boolean {
-    return this.router.url === route;
-  }
+    }
 
   // Add other component logic here
-  ngOnInit(): void {
-  }
+
 
 }
