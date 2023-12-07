f7404eb141e52f4b1140e322eff6fa3a9245ebca
Thabiso.Matsaba <thabismatsaba96@gmail.com>
2023-12-07 15:18:51 +0200

    added slide and fade logic to testiminial

--- src/app/components/testimonials/testimonials.component.ts
+++ src/app/components/testimonials/testimonials.component.ts
@@ -1,52 +1,64 @@
-import { trigger, transition, style, animate, query, animateChild, group } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
+import { trigger, transition, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-testimonials',
   templateUrl: './testimonials.component.html',
   styleUrls: ['./testimonials.component.scss'],
   animations: [
+    trigger('fade', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('500ms', style({ opacity: 1 })),
+      ]),
+      transition(':leave', [
+        animate('500ms', style({ opacity: 0 })),
+      ]),
+    ]),
     trigger('slide', [
-      transition(':increment', [
-        group([
-          query(':enter', [
-            style({ transform: 'translateX(100%)' }),
-            animate('500ms ease-in-out', style({ transform: 'translateX(0)' })),
-          ]),
-          query(':leave', [
-            animate('500ms ease-in-out', style({ transform: 'translateX(-100%)' })),
-          ]),
-        ]),
-        animateChild(),
-      ]),
-      transition(':decrement', [
-        group([
-          query(':enter', [
-            style({ transform: 'translateX(-100%)' }),
-            animate('500ms ease-in-out', style({ transform: 'translateX(0)' })),
-          ]),
-          query(':leave', [
-            animate('500ms ease-in-out', style({ transform: 'translateX(100%)' })),
-          ]),
-        ]),
-        animateChild(),
-      ]),
+      transition('* => *', [
+        style({ transform: 'translateX({{ from }})' }),
+        animate('500ms ease-in-out', style({ transform: 'translateX({{ to }})' })),
+      ], { params: { from: '100%', to: '0%' } })
     ]),
   ],
 })
 export class TestimonialsComponent implements OnInit {
-  testimonials = [
-    // Your testimonial data goes here
+  testimonials: any[] = [
+    {
+      quote: "Without a doubt Thabiso is one of the talented programmers, trustworthy and professional. His advice has helped me to grow as well in the field.",
+      image: "assets/images/Tiyani.jpeg",
+      name: "Tiyani Baloyi",
+      designation: "Senior Software Engineer, South African Radio and Astronomy Observatory (SARAO)"
+    },    
+    {
+      quote: "Thabiso is a good programmer, great team-player and works well under pressure.",
+      image: "assets/images/Luvo.PNG",
+      name: "Luvo Ntshwela",
+      designation: "Junior Software Developer, Entelect"
+    },
+    {
+      quote: "Without a doubt Thabiso is one of the talented programmers, trustworthy and professional. His advice has helped me to grow as well in the field.",
+      image: "assets/images/Vuyo.jpg",
+      name: "Vuyolwethu Mfidi",
+      designation: "Junior Software Developer, Uvu Africa:CapaCiti"
+    },
   ];
 
   selectedIndex = 0;
 
   ngOnInit(): void {
-    // Set up your testimonial data
-    // ...
+    window.onload = () => {
+      this.showTestimonial();
 
-    // You can also implement an automatic slider using setInterval
-    setInterval(() => this.nextTestimonial(), 5000); // Change slide every 5 seconds
+      // Switch to the next testimonial after 5 seconds
+      setTimeout(() => this.nextTestimonial(), 5000);
+    };
+  }
+
+  showTestimonial(): void {
+    // Initially show the first testimonial at the center
+    this.selectedIndex = 0;
   }
 
   selectTestimonial(index: number): void {
@@ -54,16 +66,17 @@ export class TestimonialsComponent implements OnInit {
   }
 
   nextTestimonial(): void {
+    // Move to the next testimonial
     this.selectedIndex = (this.selectedIndex + 1) % this.testimonials.length;
+
+    // Switch to the next testimonial after 5 seconds
+    setTimeout(() => this.nextTestimonial(), 5000);
   }
 
-  prevTestimonial(): void {
-    this.selectedIndex = (this.selectedIndex - 1 + this.testimonials.length) % this.testimonials.length;
-  }
-
-  slide() {
+  slide(from: number, to: number) {
     return {
-      value: this.selectedIndex,
+      value: 0,
+      params: { from, to }
     };
-  }
+  }  
 }
