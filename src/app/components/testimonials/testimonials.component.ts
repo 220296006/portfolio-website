87ebba039deda072dbce158130c9674ff43d5cd2
Thabiso.Matsaba <thabismatsaba96@gmail.com>
2023-12-08 16:11:19 +0200

    added new styling to testimonial

--- src/app/components/testimonials/testimonials.component.ts
+++ src/app/components/testimonials/testimonials.component.ts
@@ -8,33 +8,51 @@ import { trigger, transition, style, animate } from '@angular/animations';
   templateUrl: './testimonials.component.html',
   styleUrls: ['./testimonials.component.scss'],
   animations: [
+    trigger('fade', [
+      transition(':increment', [
+        style({ opacity: 1 }),
+        animate('500ms', style({ opacity: 0 })),
+      ]),
+      transition(':decrement', [
+        style({ opacity: 1 }),
+        animate('500ms', style({ opacity: 0 })),
+      ]),
+    ]),
     trigger('slide', [
-      transition('* => *', [
-        style({ transform: 'translateX({{ from }}%)' }),
-        animate('500ms ease-in-out', style({ transform: 'translateX({{ to }}%)' })),
-      ], { params: { from: 0, to: 0 } })
+      transition(':increment', [
+        style({ transform: 'translateX(100%)' }),
+        animate('500ms', style({ transform: 'translateX(0%)' })),
+      ]),
+      transition(':decrement', [
+        style({ transform: 'translateX(-100%)' }),
+        animate('500ms', style({ transform: 'translateX(0%)' })),
+      ]),
     ]),
   ],
 })
 export class TestimonialsComponent implements OnInit {
   testimonials: any[] = [
     {
-      quote: "Mr Matsaba, is not only smart, hardoworking and dedicated, but he proved that in his work also.",
-      image: "assets/images/Tiyani.jpeg",
-      name: "Tiyani Baloyi",
-      designation: "Senior Software Engineer, South African Radio and Astronomy Observatory (SARAO)"
+      quote:
+        'Mr Matsaba, is not only smart, hardworking and dedicated, but he proved that in his work also.',
+      image: 'assets/images/Tiyani.jpeg',
+      name: 'Tiyani Baloyi',
+      designation:
+        'Senior Software Engineer, South African Radio and Astronomy Observatory (SARAO)',
     },
     {
-      quote: "Thabiso is a good programmer, great team-player and works well under pressure.",
-      image: "assets/images/Luvo.PNG",
-      name: "Luvo Ntshwela",
-      designation: "Junior Software Developer, Entelect"
+      quote:
+        'Thabiso is a good programmer, great team-player and works well under pressure.',
+      image: 'assets/images/Luvo.PNG',
+      name: 'Luvo Ntshwela',
+      designation: 'Junior Software Developer, Entelect',
     },
     {
-      quote: "Without a doubt Thabiso is one of the talented programmers, trustworthy and professional. His advice has helped me to grow as well in the field.",
-      image: "assets/images/Vuyo.jpg",
-      name: "Vuyolwethu Mfidi",
-      designation: "Junior Software Developer, Uvu Africa:CapaCiti"
+      quote:
+        'Without a doubt Thabiso is one of the talented programmers, trustworthy and professional. His advice has helped me to grow as well in the field.',
+      image: 'assets/images/Vuyo.jpg',
+      name: 'Vuyolwethu Mfidi',
+      designation: 'Junior Software Developer, Uvu Africa:CapaCiti',
     },
   ];
 
@@ -51,12 +69,5 @@ export class TestimonialsComponent implements OnInit {
 
   nextTestimonial(): void {
     this.selectedIndex = (this.selectedIndex + 1) % this.testimonials.length;
-  }
-
-  slide() {
-    const from = this.selectedIndex === 0 ? (this.testimonials.length - 1) * 100 : (this.selectedIndex - 1) * 100;
-    const to = this.selectedIndex * 100;
-
-    return { value: 0, params: { from, to } };
   }
 }
